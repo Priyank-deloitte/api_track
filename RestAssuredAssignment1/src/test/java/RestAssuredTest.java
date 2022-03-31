3c5db21010071300d522287a2f88b24aa0f98ec9
Verma <priyankverma@deloitte.com>
2022-03-31 14:08:18 +0530

    rest assignment 1 added

--- RestAssuredAssignment1/src/test/java/RestAssuredTest.java
+++ RestAssuredAssignment1/src/test/java/RestAssuredTest.java
@@ -1,18 +1,17 @@
-import com.sun.org.apache.xerces.internal.util.PropertyState;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONArray;
 import org.testng.annotations.Test;
-
 import java.io.File;
-
-import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matcher.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-
 public class RestAssuredTest {
+
+    private RequestSpecification given;
 
     @Test
     public void get_call(){
@@ -21,27 +20,42 @@ public class RestAssuredTest {
                 header("content type","applicaion/json").
                 when().
                 get("/posts").
-                then().body("id", hasItems(1,2),
-                        "[39].id)", is(equalTo(4))).
+                then().
                 statusCode(200);
-
-
     }
 
+    @Test
+    public void get_verify(){
+
+        Response response= given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/posts")
+                .then().extract().response();
+        JSONArray arr= new JSONArray(response.asString());
+        Object id= arr.getJSONObject(39).get("id");
+        Object userId= arr.getJSONObject(39).get("userId");
+        System.out.println("User with "+id+"have userId= "+userId);
+
+        for(int i =0; i <arr.length();i++){
+            Object obj= arr.getJSONObject(i).get("title");
+            System.out.println(obj.getClass().getName());
+        }
+    }
 
     @Test
     public void put_call(){
-        File jsonData= new File("src//test//resources//PutData.json");
-        given().
-                baseUri("https://jsonplaceholder.typicode.com").
-                body(jsonData).
-                header("content type","application/json").
-                when().
-                put("/posts").
-                then().
-                statusCode(200);
+        File jsonData= new File("C:\\Users\\priyankverma\\IdeaProjects\\RestAssuredAssignment1\\src\\test\\resources\\PutData.json");
+        given()
+                .baseUri("https://reqres.in/api").
+                body(jsonData)
+                .header("Content-Type","application/json")
+                .when()
+                .put("/users")
+                .then()
+                .statusCode(200)
+                .contentType("application/json")
+                .body("name", equalTo("Arun"))
+                .body("job",equalTo("Manager"));
 
     }
-
-
-    }
+}
